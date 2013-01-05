5c9ba0f5f70a3882d3524874a3c99342dc3ebae1
candidosales <candidosg@gmail.com>
2013-01-05 18:44:07 -0300

    Layout novo

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,8 @@
 //= require jquery_ujs
 //= require jquery.maskedinput-1.3.min
 //= require jquery.price_format.1.7.min
+//= require bootstrap.js
+//= require jquery.cookie.js
 
 //= require jquery.ui.core
 //= require jquery.ui.widget
@@ -23,5 +25,58 @@
 //= require jquery.ui.datepicker
 //= require jquery.ui.datepicker-pt-BR
 
-//= require foundation
+//= require bootstrap-wysihtml5/lib/js/wysihtml5-0.3.0.min 
+
+//= require bootstrap-datepicker/js/bootstrap-datepicker
+//= require bootstrap-timepicker/js/bootstrap-timepicker
+//= require bootstrap-colorpicker/js/bootstrap-colorpicker
+//= require bootstrap-daterangepicker/js/date
+//= require bootstrap-daterangepicker/js/bootstrap-daterangepicker
+//= require bootstrap-fileupload/js/bootstrap-fileupload
+//= require bootstrap-rowlink/js/bootstrap-rowlink
+//= require bootstrap-progressbar/js/bootstrap-progressbar
+//= require bootstrap-wysihtml5/src/bootstrap-wysihtml5
+//= require bootstrap-select/bootstrap-select
+//= require bootstrap-bootbox/bootbox.min
+//= require bootstrap-modal/js/bootstrap-modalmanager
+//= require bootstrap-modal/js/bootstrap-modal
+//= require bootstrap-wizard/js/bootstrap-wizard
+//= require bootstrap-toggle-buttons/js/bootstrap-toggle-buttons
+
+//= require google-code-prettify/prettify 
+//= require nicescroll/jquery.nicescroll.min 
+//= require qtip2/dist/jquery.qtip.min 
+//= require list/js/list.min 
+//= require list/plugins/list.paging.min 
+//= require fullcalendar/fullcalendar 
+
+//= require uniform/jquery.uniform 
+//= require select2/select2.min 
+//= require counter/jquery.counter 
+//= require elastic/jquery.elastic 
+//= require inputmask/jquery.inputmask 
+//= require inputmask/jquery.inputmask.extensions 
+//= require validate/js/jquery.validate.min 
+//= require xbreadcrumbs/xbreadcrumbs 
+
+//= require sparkline/jquery.sparkline 
+//= require flot/jquery.flot 
+//= require flot/jquery.flot.categories 
+//= require flot/jquery.flot.grow 
+//= require flot/jquery.flot.orderBars 
+//= require flot/jquery.flot.pie 
+//= require flot/jquery.flot.resize 
+//= require flot/jquery.flot.selection 
+//= require flot/jquery.flot.stack 
+//= require flot/jquery.flot.stackpercent 
+//= require flot/jquery.flot.time 
+
+//= require datatables/media/js/jquery.dataTables 
+//= require datatables/plugin/jquery.dataTables.plugins 
+//= require datatables/plugin/jquery.dataTables.columnFilter  
+
+//= require main
+
+//= require demo/demo-dashboard1  
+
 //= require_tree .
