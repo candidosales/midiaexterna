07c011703b8712a2feeba87cb94a6fe4021feb65
candidosales <candidosg@gmail.com>
2013-02-05 17:28:07 -0300

    Config puma

--- config/puma.rb
+++ config/puma.rb
@@ -3,8 +3,8 @@ rails_env = ENV['RAILS_ENV'] || 'development'
 threads 4,4
 
 bind  "unix:///var/www/midiaexterna/shared/tmp/puma/midiaexterna-puma.sock"
-pidfile "/var/www/midiaexterna/current/tmp/puma/pid"
-state_path "/var/www/midiaexterna/current/tmp/puma/state"
+pidfile "/var/www/midiaexterna/tmp/puma/pid"
+state_path "/var/www/midiaexterna/tmp/puma/state"
 
 activate_control_app
 
