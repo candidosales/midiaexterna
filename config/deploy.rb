c166608b9f32d0d7d09c12020aee7f3adf26bf53
candidosales <candidosg@gmail.com>
2013-02-06 15:35:21 -0300

    ok

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,37 +1,26 @@
 # ==============================================================
 # SET's
 # ==============================================================
-
-set :application, "midiaexterna"
-set :rails_env, :production
-
-set :scm, :git
-set :repository, "git@github.com:candidosales/midiaexterna.git"
-set :branch, "master"
-
 set :domain, "177.71.250.61"
-set :rvm_ruby_string, "1.9.3"
+set :application, "midiaexterna"
+set :repository, "git@github.com:candidosales/midiaexterna.git"
+
 
 default_run_options[:pty] = true
-
 ssh_options[:verbose] = :debug
-ssh_options[:keys] = "~/.ec2/midiaexterna-ec2.pem"
 ssh_options[:forward_agent] = true
-# you may, or may not need to use this - but let's use it just in case
-ssh_options[:paranoid] = false
-# ssh_options[:config] = false
 
 set :user, "ubuntu"
-set :runner, "ubuntu"
-set :group, "ubuntu"
-set :use_sudo, false
+set :use_sudo, true
+set :scm, :git
 set :keep_releases, 5
+set :branch, "master"
 
 #Uma ótima opção referente ao Github que podemos adicionar é o remote_chache. 
 #Esta opção evita que seja feito o clone de todo o repositório a cada deploy. 
 #Ao invés disso, é feito apenas um fetch das alterações, deixando assim, o deploy bem mais rápido:
-set :deploy_via, :remote_cache
-set :deploy_to, "/var/www/#{application}"
+#set :deploy_via, :remote_cache
+set :deploy_to, "/home/ubuntu/#{application}"
 set :current, "#{deploy_to}/current"
 
 
